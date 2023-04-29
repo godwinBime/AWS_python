af5afa8e50dd0459cd9cc53c4c2bf6f7dc30415c
Godwin Bime <bimegodwin@cityu.edu>
2023-04-28 21:58:01 -0400

    Object detection project

---
+++ ObjectDetectionDemo/Object_detection_demo.py
@@ -0,0 +1,12 @@
+from storage_service import StorageService
+from recognition_service import RecognitionService
+
+storage_service = StorageService()
+recognition_service = RecognitionService()
+bucket_name = 'bucketone2023'
+for file in storage_service.get_all_files(bucket_name):
+    if file.key.endswith('.jpg'):
+        print('Objects detected in image ' + file.key + ':')
+        labels = recognition_service.detect_objects(file.bucket_name, file.key)
+        for label in labels:
+            print('-- ' + label['Name'] + ': ' + str(label['Confidence']))
